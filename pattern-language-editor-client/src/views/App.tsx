088c98e82c4fda459130ec415ddb9e9222e11b5f
msntts <terzonome-github@yahoo.co.jp>
2023-03-05 15:20:10 +0900

    Web画面に項目を表示する の実装
    - App.tsxに動作確認のために値を入れた状態。(将来的には消す)
    - 今はreadonly。将来的に入力画面とパターンのビューの画面が違うものになるかもしれないので、まずはビューだけ

--- pattern-language-editor-client/src/views/App.tsx
+++ pattern-language-editor-client/src/views/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './App.css'
+import Pattern from './components/Pattern'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
+      <Pattern
+        name="おしゃれな見た目"
+        imgPath=""
+        context="自分たちのコンテンツを展示し、興味を持ってもらおうとしている"
+        problem="他にもたくさんの展示があり、自分の展示が埋もれてしまう"
+        fource="見てくれる全員を捕まえて詳しい説明をする時間がない"
+        solution="コンテンツの入り口となる部分の見た目をよくして興味を持ってもらおう"
+        result="自分たちのコンテンツに興味を持ってもらうきっかけを増やせる"
+      ></Pattern>
     </div>
   )
 }
